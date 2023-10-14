8ea82e33f29f91bd4e1592448aa284c426dffd99
theanh098 <mtheanh2910@gmail.com>
2023-10-14 21:13:58 +0700

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+  let app = Router::new().route("/", get(root));
+
+  axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
+}
+
+async fn root() -> &'static str {
+  "Hello, World!"
+}
