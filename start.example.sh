b4f55bf3fa6e763b14b6f86503b226bf734d3486
theanh098 <mtheanh2910@gmail.com>
2023-11-21 21:21:51 +0700

    sample

---
+++ start.example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DATABASE_URL=postgresql://postgres:vitaminc@localhost:5432/luxon
+export ALCHEMY_API_KEY=zWxiWD0PIDFyC_V8FO4nhGVqYyT4u1AC
+export OPENSEA_API_KEY=c37820178cd54ea5b81ef9c51930ce4e
+export JWT_SECRET=big_tits_lover
+export JWT_REFRESH_SECRET=milf_lover
+
+
+/home/ubuntu/nft-app/nft-adc-platform
